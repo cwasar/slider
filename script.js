08db86f3db5645793c4f61c799b9fd62b96279b5
cwasar <65034502+cwasar@users.noreply.github.com>
2021-06-17 12:19:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const cont = document.querySelector('.container')
 const slides = document.querySelectorAll('.slide')
+const over = document.querySelector('#overlay')
 
 cont.addEventListener('click', slider)
 
 function slider(event) {
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].classList.remove('active')
+	console.log(event.target);
+	if (!(event.target.classList.contains('active'))) {
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].classList.remove('active')
+		}
 		event.target.classList.add('active')
+
+	} else if (event.target.classList.contains('active')) {
+		event.target.classList.toggle('full')
+		over.classList.toggle('overlay')
+
 	}
+
+
 }
+
